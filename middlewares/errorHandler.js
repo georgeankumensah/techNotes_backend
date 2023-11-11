c176840a2818ddf5fc1c86ac973edaeb3ae2f6e7
georgeankumensah <georgeankumensah@gmail.com>
2023-11-11 13:52:11 +0000

    added cors configuration

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,15 @@
-const {logEvents} = require('./logger');
+const { logEvents } = require("./logger");
 
-const errorHandler = (err,req,res,next)=>{
-    logEvents(`${err.name}\t${err.message}\t${err.method}\t${err.url}\t${err.headers.origin}`,'errorLog.log');
-    console.log(err.stack);
-    const status = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(status);
-    res.json({message:err.message});
-    // process.exit(1);
-}
+const errorHandler = (err, req, res, next) => {
+  logEvents(
+    `${err.name}\t${err.message}\t${err.method}\t${err.url}\t${err.headers?.origin}`,
+    "errorLog.log"
+  );
+  console.log(err.stack);
+  const status = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(status);
+  res.json({ message: err.message });
+  // process.exit(1);
+};
 
 module.exports = errorHandler;
--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const express = require('express');
+const express = require("express");
 
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const {logger} = require('./middlewares/logger');
-const errorHandler = require('./middlewares/errorHandler');
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { logger } = require("./middlewares/logger");
+const errorHandler = require("./middlewares/errorHandler");
+const corsOptions = require("./config/corsOptions");
 
 const app = express();
 
 const PORT = process.env.PORT || 3500;
 
-
 // middlewares
-app.use(logger)
+app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.use('/',require('./routes/root'));
+app.use("/", require("./routes/root"));
 
-
-app.all('*',(req,res)=>{
-        res.status(404)
-        if (req.accepts('html')) {
-            // handle wrong page routes
-            res.sendFile(path.join(__dirname,"views",'404.html'));
-        }else if(req.accepts('json')){
-            // handle wrong api routes
-            res.json({message:"404 Not found"});
-        }else{
-            // default response type
-            res.type('txt').send('404 Not found');
-        }
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    // handle wrong page routes
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    // handle wrong api routes
+    res.json({ message: "404 Not found" });
+  } else {
+    // default response type
+    res.type("txt").send("404 Not found");
+  }
 });
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
-    }
-);
+  console.log(`Server listening on port ${PORT}...`);
+});
